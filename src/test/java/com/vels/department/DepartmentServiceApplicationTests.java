5d468c24edf6a8d24bffe17082f2c6adaf514ab7
vdhanapathy <vdhanapathy@ntst.com>
2021-10-03 09:25:11 +0530

    Initial commit for department service

---
+++ src/test/java/com/vels/department/DepartmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vels.department;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartmentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
